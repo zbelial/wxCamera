24ab1df59283b0d40a86c2dd0ba671f4d59d45af
numberwolf <porschegt23@foxmail.com>
2016-05-22 16:45:59 +0800

    test

--- ddbe/api.php
+++ ddbe/api.php
@@ -5,5 +5,10 @@ include_once('be.class.php');
 $be = \classes\be::getInstance();
 // echo $be->getAccessToken();
 
+$action = $_GET['action'];
 
+if ($action == 'getuserinfo') {
+
+	echo $_GET['code'];
+}
 
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 include('ddconfig/wxconfig.php');
 
-$callbackURL = urlencode("http://numberwolf.top/wxCamera/ddbe/code.php");
+$callbackURL = urlencode("http://numberwolf.top/wxCamera/ddbe/api.php?action=getuserinfo");
 
 header("Location: https://open.weixin.qq.com/connect/oauth2/authorize?appid=".AppID."&redirect_uri=$callbackURL&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect");
 
