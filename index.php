651fd8ee4a01e99b61c916cd0508e1fb33331064
numberwolf <porschegt23@foxmail.com>
2016-05-22 16:39:09 +0800

    update

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 include('ddconfig/wxconfig.php');
 
-$callbackURL = urlencode("https://numberwolf.top/wxCamera/ddbe/code.php");
+$callbackURL = urlencode("http://numberwolf.top/wxCamera/ddbe/code.php");
 
 header("Location: https://open.weixin.qq.com/connect/oauth2/authorize?appid=".AppID."&redirect_uri=$callbackURL&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect");
 
