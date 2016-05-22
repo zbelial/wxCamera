dad0058fdc9450ac399d421c857789bde8a816e1
numberwolf <porschegt23@foxmail.com>
2016-05-22 16:20:51 +0800

    update

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include('ddconfig/wxconfig.php');
 
 $callbackURL = urlencode("https://www.baidu.com");
 
-header("Location: https://open.weixin.qq.com/connect/oauth2/authorize?appid=".AppID."&redirect_uri=$callbackURL&response_type=code&scope=SCOPE#wechat_redirect");
+header("Location: https://open.weixin.qq.com/connect/oauth2/authorize?appid=".AppID."&redirect_uri=$callbackURL&response_type=code&scope=snsapi_userinfo#wechat_redirect");
 
 
 
