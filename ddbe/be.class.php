c46f00489ae3e91bd7093b426efbf180249ad175
numberwolf <porschegt23@foxmail.com>
2016-05-23 00:57:21 +0800

    test

--- ddbe/be.class.php
+++ ddbe/be.class.php
@@ -59,7 +59,7 @@ class be extends \db
         //http://file.api.weixin.qq.com/cgi-bin/media/get?access_token=ACCESS_TOKEN&media_id=MEDIA_ID
 
         $url = "http://file.api.weixin.qq.com/cgi-bin/media/get?access_token=$access_token&media_id=$serverId";
-        $filename = time().'jpg';
+        $filename = time().'.jpg';
 
         $res = $this->GrabImage($url,$filename,'ddstorage/');
 
