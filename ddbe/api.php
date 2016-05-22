1b105c4b41cf4de268de0feaa60afbb9b4233a28
numberwolf <porschegt23@foxmail.com>
2016-05-22 21:48:05 +0800

    test

--- ddbe/api.php
+++ ddbe/api.php
@@ -7,11 +7,29 @@ $be = \classes\be::getInstance();
 
 $action = $_GET['action'];
 
-if ($action == 'getuserinfo') {
+// if ($action == 'getuserinfo') {
 
-	$code = $_GET['code'];
-	$getOpenidURL = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=".AppID."&secret=".AppSecret."&code=$code&grant_type=authorization_code";
+// 	$code = $_GET['code'];
+// 	$getOpenidURL = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=".AppID."&secret=".AppSecret."&code=$code&grant_type=authorization_code";
 
-	$be->getUserInfo($getOpenidURL);
+// 	$be->getUserInfo($getOpenidURL);
+// }
+
+switch ($action) {
+	case 'getuserinfo':
+		$code = $_GET['code'];
+		$getOpenidURL = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=".AppID."&secret=".AppSecret."&code=$code&grant_type=authorization_code";
+
+		$be->getUserInfo($getOpenidURL);
+		break;
+
+	case 'takeuserinfo':
+		session_start();
+		$returnArr = array('nickname' => $_SESSION['nickname'], 'sex' => $_SESSION['sex'],'img' => $_SESSION['headimgurl']);
+		echo json_encode($returnArr);
+		break;
+	
+	default:
+		break;
 }
 
