6bb1ea581a2e986627fa5b1b9b120207918abaf8
numberwolf <porschegt23@foxmail.com>
2016-05-22 16:28:10 +0800

    update

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php 
 include('ddconfig/wxconfig.php');
 
-$callbackURL = urlencode("https://www.baidu.com");
+$callbackURL = urlencode("http://numberwolf.top/wxCamera/ddbe/api.php");
 
-header("Location: https://open.weixin.qq.com/connect/oauth2/authorize?appid=".AppID."&redirect_uri=$callbackURL&response_type=code&scope=snsapi_base#wechat_redirect");
+header("Location: https://open.weixin.qq.com/connect/oauth2/authorize?appid=".AppID."&redirect_uri=$callbackURL&response_type=code&scope=snsapi_userinfo#wechat_redirect");
 
 
 
